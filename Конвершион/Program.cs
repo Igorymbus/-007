4a60ac5ddd5b0f2d52fb0b2550ca883665e7db09
Igorrrrr <144235620+Igorymbus@users.noreply.github.com>
2023-11-09 09:28:24 +0300

    Update Program.cs

--- Конвершион/Program.cs
+++ Конвершион/Program.cs
@@ -1,32 +1,70 @@
-﻿using TextEditor;
+    using TextEditor;
 
-Console.WriteLine("Введите путь к файлу: ");
-string filePath = Console.ReadLine();
-FileHandler fileManager = new FileHandler(filePath);
+    static class Program
+    {
+        static void Main()
+        {
+            FileHandler fileManager = ObtainPathAndLoadFile();
+            Figure loadedFigure = fileManager?.LoadFile();
 
-Figure loadedFigure = fileManager.LoadFile();
-if (loadedFigure != null)
-{
-    Console.WriteLine("Loaded Figure:");
-    Console.WriteLine($"Name: {loadedFigure.Name}");
-    Console.WriteLine($"Width: {loadedFigure.Width}");
-    Console.WriteLine($"Height: {loadedFigure.Height}");
-}
-else
-{
-    Console.WriteLine("Ошибка загрузки файла.");
-}
+            if (loadedFigure != null)
+            {
+                DisplayLoadedFigure(loadedFigure);
+                PerformFileOperationsLoop(fileManager, loadedFigure);
+            }
+            else
+            {
+                Console.WriteLine("Ошибка загрузки файла.");
+            }
+        }
 
-ConsoleKeyInfo keyInfo;
-do
-{
-    Console.WriteLine("Нажмите F1, чтобы сохранить в новом формате, или Esc для выхода.");
-    keyInfo = Console.ReadKey();
-    if (keyInfo.Key == ConsoleKey.F1)
+        static FileHandler ObtainPathAndLoadFile()
+        {
+            Console.Write("Введите путь к файлу: ");
+        string filePath = Console.ReadLine();
+
+        return new FileHandler(filePath);
+    }
+
+    static void DisplayLoadedFigure(Figure loadedFigure)
+    {
+        Console.WriteLine("Loaded Figure:");
+        Console.WriteLine($"Name: {loadedFigure.Name}");
+        Console.WriteLine($"Width: {loadedFigure.Width}");
+        Console.WriteLine($"Height: {loadedFigure.Height}");
+    }
+
+    static void PerformFileOperationsLoop(FileHandler fileManager, Figure loadedFigure)
+    {
+        ConsoleKeyInfo keyInfo;
+        do
+        {
+            Console.WriteLine("Нажмите F1, чтобы сохранить в новом формате, или Esc для выхода.");
+            keyInfo = Console.ReadKey(true);
+            if (keyInfo.Key == ConsoleKey.F1)
+            {
+                string newFormat = RequestNewFileFormat();
+                if (!string.IsNullOrEmpty(newFormat))
+                {
+                    fileManager.SaveFile(loadedFigure, newFormat);
+                    Console.WriteLine($"Файл сохранён в формате {newFormat}.");
+                }
+            }
+        } while (keyInfo.Key != ConsoleKey.Escape);
+    }
+
+    static string RequestNewFileFormat()
     {
         Console.WriteLine("Введите новый формат файла (txt, json, xml):");
         string newFormat = Console.ReadLine();
-        fileManager.SaveFile(loadedFigure, newFormat);
-        Console.WriteLine($"Файл сохранён в формате {newFormat}.");
+        if (newFormat.Equals("txt", StringComparison.OrdinalIgnoreCase) ||
+            newFormat.Equals("json", StringComparison.OrdinalIgnoreCase) ||
+            newFormat.Equals("xml", StringComparison.OrdinalIgnoreCase))
+        {
+            return newFormat;
+        }
+
+        Console.WriteLine("Введён недопустимый формат файла.");
+        return null;
     }
-} while (keyInfo.Key != ConsoleKey.Escape);
+}
